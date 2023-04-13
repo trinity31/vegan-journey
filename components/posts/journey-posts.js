3830b68a5ed077e69225b6a54fd2bcaf03129cd5
trinity31 <kyeongyeon.song@gmail.com>
2023-04-13 23:04:41 +0900

    meta data for journey, recipe list page

--- components/posts/journey-posts.js
+++ components/posts/journey-posts.js
@@ -1,25 +1,36 @@
 import Container from "../container";
 import { useTranslation } from "next-i18next";
 import JourneyItem from "./journey-item";
+import Head from "next/head";
 
 export default function JourneyPosts(props) {
   const { posts } = props;
   const { t } = useTranslation("common");
 
   return (
-    <Container>
-      <h1 className="text-3xl font-bold mb-6 text-green-900">
-        {t("journey_posts_title")}
-      </h1>
-      <h3 className="text-xl font-medium mb-4 text-gray-700">
-        {t("journey_posts_desc")}
-      </h3>
-      <div className="grid grid-cols-1 py-5 sm:grid-cols-2 lg:grid-cols-2 gap-6">
-        {posts &&
-          posts.map((post, index) => (
-            <JourneyItem key={post.slug} post={post} index={index} />
-          ))}
-      </div>
-    </Container>
+    <>
+      <Head>
+        <title>{t("journey_posts_title")}</title>
+        <meta name="description" content={t("journey_posts_desc")} />
+        <meta property="og:title" content={t("journey_posts_title")} />
+        <meta property="og:description" content={t("journey_posts_desc")} />
+        <meta property="og:image" content={posts[0].image} />
+        <meta property="og:type" content="blog"></meta>
+      </Head>
+      <Container>
+        <h1 className="text-3xl font-bold mb-6 text-green-900">
+          {t("journey_posts_title")}
+        </h1>
+        <h3 className="text-xl font-medium mb-4 text-gray-700">
+          {t("journey_posts_desc")}
+        </h3>
+        <div className="grid grid-cols-1 py-5 sm:grid-cols-2 lg:grid-cols-2 gap-6">
+          {posts &&
+            posts.map((post, index) => (
+              <JourneyItem key={post.slug} post={post} index={index} />
+            ))}
+        </div>
+      </Container>
+    </>
   );
 }
--- components/posts/recipe-posts.js
+++ components/posts/recipe-posts.js
@@ -1,26 +1,37 @@
 import Container from "../container";
 import { useTranslation } from "next-i18next";
 import RecipeItem from "./recipe-item";
+import Head from "next/head";
 
 export default function RecipePosts(props) {
   const { posts } = props;
   const { t } = useTranslation("common");
 
   return (
-    <Container>
-      <h1 className="text-3xl font-bold mb-6 text-green-900">
-        {t("recipe_posts_title")}
-      </h1>
-      <h3 className="text-xl font-medium mb-4 text-gray-700">
-        {t("recipe_posts_desc")}
-      </h3>
-      {/* <PostsGrid posts={props.posts}></PostsGrid> */}
-      <div className="grid grid-cols-1 py-5 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {posts &&
-          posts.map((post, index) => (
-            <RecipeItem key={post.slug} post={post} index={index} />
-          ))}
-      </div>
-    </Container>
+    <>
+      <Head>
+        <title>{t("recipe_posts_title")}</title>
+        <meta name="description" content={t("recipe_posts_desc")} />
+        <meta property="og:title" content={t("recipe_posts_title")} />
+        <meta property="og:description" content={t("recipe_posts_desc")} />
+        <meta property="og:image" content={posts[0].image} />
+        <meta property="og:type" content="blog"></meta>
+      </Head>
+      <Container>
+        <h1 className="text-3xl font-bold mb-6 text-green-900">
+          {t("recipe_posts_title")}
+        </h1>
+        <h3 className="text-xl font-medium mb-4 text-gray-700">
+          {t("recipe_posts_desc")}
+        </h3>
+        {/* <PostsGrid posts={props.posts}></PostsGrid> */}
+        <div className="grid grid-cols-1 py-5 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {posts &&
+            posts.map((post, index) => (
+              <RecipeItem key={post.slug} post={post} index={index} />
+            ))}
+        </div>
+      </Container>
+    </>
   );
 }
